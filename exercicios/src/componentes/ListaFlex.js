3e60d24bff21d58318acc703b9eb379a14e2370a
Marks <marks@prodata.inf.br>
2019-10-14 07:56:21 -0300

    Comecando aula de Lista de Alunos. Implementado arraylist dos alunos, para trabalhar componente. Ideia de montar componente de listagem.

---
+++ exercicios/src/componentes/ListaFlex.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ScrollView, View, FlatList, Text} from 'react-native'
+
+const alunos = [
+    {id: 1, nome: 'Aluno 1', nota: 7.9},
+    {id: 2, nome: 'Aluno 2', nota: 7.9},
+    {id: 3, nome: 'Aluno 3', nota: 7.9},
+    {id: 4, nome: 'Aluno 4', nota: 7.9},
+    {id: 5, nome: 'Aluno 5', nota: 7.9},
+    {id: 6, nome: 'Aluno 6', nota: 7.9},
+    {id: 7, nome: 'Aluno 7', nota: 7.9},
+    {id: 8, nome: 'Aluno 8', nota: 7.9},
+    {id: 9, nome: 'Aluno 9', nota: 7.9},
+]
