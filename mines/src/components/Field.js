84456b74a1d9ba3578694a06403305a5545fdf55
Marks <marks@prodata.inf.br>
2019-11-13 08:01:44 -0300

    Aula 52: Componente Bandeira (Flag) #1: Criado componente e adicionado no Field a rendereizacao condicional. Adicionado no componente App utilizacao do mesmo. Ajustado documentacao para ter codigo de inicializacao do emulador local pronto, ao inves de pesquisar e utilizar.

--- mines/src/components/Field.js
+++ mines/src/components/Field.js
@@ -2,15 +2,18 @@ import React from 'react'
 import {View, StyleSheet, Text} from 'react-native'
 import params from '../params'
 import Mine from './Mine'
+import Flag from './Flag'
 
 export default props => {
-    const {mined, opened, nearMines, exploded} = props
+    const {mined, opened, nearMines, exploded, flagged} = props
 
     const styleField = [styles.field]
     //Outros estilos por testes
     if (opened) styleField.push(styles.opened)
     if (exploded) styleField.push(styles.exploded)
-    if (styleField.length === 1) styleField.push(styles.regular)
+    if (flagged) styleField.push(styles.flagged)
+    //if (styleField.length === 1) styleField.push(styles.regular)
+    if (!opened && !exploded) styleField.push(styles.regular)
 
     let color = null
     if (nearMines > 0) {
@@ -25,6 +28,7 @@ export default props => {
             ? <Text style={[styles.label, {color: color}]}>{nearMines}</Text> 
             : false }
            {mined && opened ? <Mine /> : false}
+           {flagged && !opened ? <Flag /> : false}
         </View>
     )
 }
@@ -55,5 +59,8 @@ const styles = StyleSheet.create({
     exploded: {
         backgroundColor: 'red',
         borderColor: 'red',
+    },
+    flagged: {
+
     }
 })
