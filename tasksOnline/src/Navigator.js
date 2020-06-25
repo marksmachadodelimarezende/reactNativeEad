288d1c6099c398b10f943754c571df3c5a37c12e
Marks <marks@prodata.inf.br>
2020-06-25 08:16:54 -0300

    Criado menu Drawer com react-navigation-drawer. Foi necessário inserir metodo referente ao gestureHandler no MainActivity.java. Foi necessário instalar pacote referente ao mesmos, sendo atualizado o package.json.

--- tasksOnline/src/Navigator.js
+++ tasksOnline/src/Navigator.js
@@ -1,7 +1,41 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation'
-
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 import Auth from './screens/Auth'
 import Agenda from './screens/Agenda'
+
+const menuRoutes = {
+    Today: {
+        name: 'Today',
+        screen: props => <Agenda title='Hoje' daysAhead={0} {...props}/>,
+        navigationOptions: {
+            title: 'Hoje',
+        }
+    },
+    Tomorrow: {
+        name: 'Tomorrow',
+        screen: props => <Agenda title='Amanhã' daysAhead={1} {...props}/>,
+        navigationOptions: {
+            title: 'Amanhã',
+        }
+    },
+    Week: {
+        name: 'Week',
+        screen: props => <Agenda title='Semana' daysAhead={7} {...props}/>,
+        navigationOptions: {
+            title: 'Semana',
+        }
+    },
+    Month: {
+        name: 'Month',
+        screen: props => <Agenda title='Mês' daysAhead={30} {...props}/>,
+        navigationOptions: {
+            title: 'Mês',
+        }
+    },
+}
+
+const menuNavigator = createDrawerNavigator(menuRoutes)
 
 const mainRoute = {
     Auth: {
@@ -10,7 +44,7 @@ const mainRoute = {
     },
     Home: {
         name: 'Home',
-        screen: Agenda,
+        screen: menuNavigator,
     },
 }
 
